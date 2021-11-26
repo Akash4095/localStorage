f5933d1104b3f75ff1f60372d980003882f6ebd3
Akash4095 <akashathnure40@gmail.com>
2021-11-26 14:34:09 +0530

    added showUserOnScreen function

--- jsfile.js
+++ jsfile.js
@@ -2,7 +2,7 @@ const myForm= document.querySelector('#my-form');
 const name= document.querySelector('#name');
 const email= document.querySelector('#email');
 const msg= document.querySelector('.msg');
-const userList= document.querySelector('#user');
+const userList= document.querySelector('#users');
 
 myForm.addEventListener('submit',onSubmit);
 
@@ -16,9 +16,24 @@ function onSubmit(e){
         email:email.value
     }
     
-    const obj= JSON.stringify(userDetails)
-    localStorage.setItem('userDetails',obj)
-    
+    const obj = JSON.stringify(userDetails);
+    localStorage.setItem(userDetails.email,obj);
 
+    // var data =JSON.parse(localStorage.getItem('email'))
+    
+    showUserOnScreen(userDetails)
+    
 }
+
+function showUserOnScreen(user){
+
+    // const li= document.createElement('li');
+    // li.innerHTML=(document.createTextNode(JSON.parse(localStorage.getItem(user.name))));
+    // userList.appendChild(li)
+
+    const parentNode= document.getElementById('users');
+    const childHTML= `<li> ${user.name} -  ${user.email} </li>`
+
+    parentNode.innerHTML= parentNode.innerHTML+childHTML;
+ }
 
