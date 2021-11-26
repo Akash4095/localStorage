a97faa4e5ffbcd9896edfa69c5ae8a479071b936
Akash4095 <akashathnure40@gmail.com>
2021-11-26 18:49:06 +0530

    added edit and delete function

--- jsfile.js
+++ jsfile.js
@@ -6,6 +6,7 @@ const userList= document.querySelector('#users');
 
 myForm.addEventListener('submit',onSubmit);
 
+
 function onSubmit(e){
     e.preventDefault();
     // localStorage.setItem('name',name.value);
@@ -23,17 +24,46 @@ function onSubmit(e){
     
     showUserOnScreen(userDetails)
     
+    
 }
 
 function showUserOnScreen(user){
 
     // const li= document.createElement('li');
-    // li.innerHTML=(document.createTextNode(JSON.parse(localStorage.getItem(user.name))));
+    // li.innerHTML=document.createTextNode(JSON.parse(localStorage.getItem('userDetails')));
     // userList.appendChild(li)
-
+    if(localStorage.getItem(user.email)!==null){
+        removeUserFromTheScreen(user.email)
+    }
     const parentNode= document.getElementById('users');
-    const childHTML= `<li> ${user.name} -  ${user.email} </li>`
+    const childHTML= `<li id="${user.email}" > ${user.name} -  ${user.email} 
+                    <button  onclick=removeUser('${user.email}')>Delete User</button>
+                    <button onclick=editUser('${user.name}','${user.email}')> Edit user</button>
+                    </li>`;
 
     parentNode.innerHTML= parentNode.innerHTML+childHTML;
  }
+function removeUser(emailId){
+    localStorage.removeItem(emailId)
+    removeUserFromTheScreen(emailId)
 
+}
+
+function editUser(uname,emailId){
+    document.getElementById('name').value=uname
+    document.getElementById('email').value=emailId
+    localStorage.removeItem(emailId)
+    removeUserFromTheScreen(emailId)
+    
+}
+
+
+function removeUserFromTheScreen(emailId){
+    const parentNode= document.getElementById('users');
+    const childNodeToBeDeleted= document.getElementById(emailId)
+
+    if(childNodeToBeDeleted){
+        parentNode.removeChild(childNodeToBeDeleted)
+    }
+    
+}
